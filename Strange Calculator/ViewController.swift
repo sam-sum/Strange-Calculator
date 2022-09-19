27abaaeb22f2f9159bee0e7ed7e1850fe6707c06
Samuel Sum <sch.developer@gmail.com>
2022-09-19 17:54:01 -0400

    Initial Commit

---
+++ Strange Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Strange Calculator
+//
+//  Created by Samuel Sum on 2022-09-19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
