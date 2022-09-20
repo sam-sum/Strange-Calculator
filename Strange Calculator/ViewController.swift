c9dc84bd3421fe633446b8a885084a17c67920ff
Samuel Sum <sch.developer@gmail.com>
2022-09-20 10:03:28 -0400

    initial commit

--- Strange Calculator/ViewController.swift
+++ Strange Calculator/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnDown(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.white
+    }
+    @IBAction func btnUpOutside(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.clear
+    }
+    
+    @IBAction func btn1UpInside(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.clear
+    }
+    
+    @IBAction func btn2UpInside(_ sender: UIButton) {
+        sender.backgroundColor = UIColor.clear
+    }
+    
 }
 
